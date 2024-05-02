c85465790490728072178275f11a5315900ea8b5
DrorGaon <168739449+DrorGaon@users.noreply.github.com>
2024-05-02 17:51:03 +0300

    First commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 var gCinema
 var gElSelectedSeat = null
+var gRemovalTimeoutId
+var gElHighlitedCells
 
 function onInit() {
     gCinema = createCinema()
@@ -11,10 +13,10 @@ function onInit() {
 function createCinema() {
     const cinema = []
 
-    for (var i = 0; i < 7; i++) {
+    for (var i = 0; i < 6; i++) {
         cinema[i] = []
-        for (var j = 0; j < 15; j++) {
-            const cell = { isSeat: j !== 7 }
+        for (var j = 0; j < 10; j++) {
+            const cell = { isSeat: j !== 7 && j !== 2 && i !== 3}
             if (cell.isSeat) {
                 cell.price = 5 + i
                 cell.isBooked = false
@@ -45,7 +47,8 @@ function renderCinema() {
             const title = `Seat: ${i + 1}, ${j + 1}`
 
             strHTML += `\t<td title="${title}" class="cell ${className}" 
-                            onclick="onCellClicked(this, ${i}, ${j})" >
+                            onclick="onCellClicked(this, ${i}, ${j})"
+                            data-i="${i}" data-j="${j}">
                          </td>\n`
         }
         strHTML += `</tr>\n`
@@ -61,16 +64,21 @@ function onCellClicked(elCell, i, j) {
     // ignore none seats and booked seats
     if (!cell.isSeat || cell.isBooked) return
 
-    console.log('Cell clicked: ', elCell, i, j)
-
     // Selecting a seat
     elCell.classList.add('selected')
     
     // Only a single seat should be selected
     if (gElSelectedSeat) {
         gElSelectedSeat.classList.remove('selected')
+
     }
+
     gElSelectedSeat = (gElSelectedSeat !== elCell) ? elCell : null
+
+    if(gRemovalTimeoutId){
+        removeHighlights(gElHighlitedCells)
+        clearInterval(gRemovalTimeoutId)
+    }
 
     // When seat is selected a popup is shown
     if (gElSelectedSeat) {
@@ -97,6 +105,8 @@ function showSeatDetails(pos) {
 
 function hideSeatDetails() {
     document.querySelector('.popup').hidden = true
+    gElSelectedSeat.classList.toggle('selected')
+    gElSelectedSeat = null
 }
 
 function onBookSeat(elBtn) {
@@ -112,15 +122,47 @@ function onBookSeat(elBtn) {
 
 function countAvailableSeatsAround(board, rowIdx, colIdx) {
     var count = 0
-
     for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
         if (i < 0 || i >= board.length) continue
         for (var j = colIdx - 1; j <= colIdx + 1; j++) {
             if (i === rowIdx && j === colIdx) continue
             if (j < 0 || j >= board[0].length) continue
             var currCell = board[i][j]
-            if (currCell.isSeat && !currCell.isBooked) count++
+            if (currCell.isSeat && !currCell.isBooked){
+                count++
+            }
         }
     }
     return count
+}
+
+function highlightCells(){
+    if (gRemovalTimeoutId) clearTimeout(gRemovalTimeoutId)
+
+    gElHighlitedCells = []
+    const rowIdx = +gElSelectedSeat.dataset.i
+    const colIdx = +gElSelectedSeat.dataset.j
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= gCinema.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === rowIdx && j === colIdx) continue
+            if (j < 0 || j >= gCinema[0].length) continue
+            var currCell = gCinema[i][j]
+            if (currCell.isSeat && !currCell.isBooked){
+                var elCell = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
+                elCell.classList.add('highlight')
+                gElHighlitedCells.push(elCell)
+            } 
+        }    
+    }
+
+    gRemovalTimeoutId = setTimeout(() => {
+        removeHighlights(gElHighlitedCells)
+    }, 2500);
+}
+
+function removeHighlights(gElHighlitedCells){
+    for(var i = 0; i < gElHighlitedCells.length; i++){
+        gElHighlitedCells[i].classList.remove('highlight')
+    }
 }
